9fa6e8ef8e3668488c076d9114586b8ba246a9c7
hgf <1454556135@qq.com>
2022-08-17 23:51:38 +0800

    feat(launch): 添加核心f方法

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,40 @@
-import url from 'url'
-import path from 'path'
-
-import type { specifiedEditor, srcRoot, onErrorCallback } from './types'
+import url from "url";
+import path from "path";
+import { launch } from "./core/launch-editor";
+import type { specifiedEditor, srcRoot, onErrorCallback } from "./types";
 // const launch = require('./launch-editor')
 
-export default function (specifiedEditor: specifiedEditor, srcRoot: srcRoot, onErrorCallback: onErrorCallback) {
-  if (typeof specifiedEditor === 'function') {
-    onErrorCallback = specifiedEditor
-    specifiedEditor = undefined
+export default function (
+  specifiedEditor: specifiedEditor,
+  srcRoot: srcRoot,
+  onErrorCallback: onErrorCallback
+) {
+  if (typeof specifiedEditor === "function") {
+    onErrorCallback = specifiedEditor;
+    specifiedEditor = undefined;
   }
 
-  if (typeof srcRoot === 'function') {
-    onErrorCallback = srcRoot
-    srcRoot = undefined
+  if (typeof srcRoot === "function") {
+    onErrorCallback = srcRoot;
+    srcRoot = undefined;
   }
 
-  srcRoot = srcRoot || process.cwd()
+  srcRoot = srcRoot || process.cwd();
 
   return function launchEditorMiddleware(req: any, res: any) {
-    const { file } = url.parse(req.url, true).query || {}
+    const { file } = url.parse(req.url, true).query || {};
     if (!file) {
-      res.statusCode = 500
-      res.end(`launch-editor-middleware: required query param "file" is missing.`)
+      res.statusCode = 500;
+      res.end(
+        `launch-editor-middleware: required query param "file" is missing.`
+      );
     } else {
-      launch(path.resolve(srcRoot as string, file as string), specifiedEditor, onErrorCallback)
-      res.end()
+      launch(
+        path.resolve(srcRoot as string, file as string),
+        specifiedEditor,
+        onErrorCallback
+      );
+      res.end();
     }
-  }
+  };
 }
