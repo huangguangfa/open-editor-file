a7455c1e2082bd4c7a6a98824ca9b9d107dd6a85
hgf <1454556135@qq.com>
2022-08-19 19:25:57 +0800

    feat(vue2):添加vue2测试项目

---
+++ examples/vue2/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <componentA></componentA>
+    <componentB></componentB>
+  </div>
+</template>
+
+<script>
+import componentA from "./components/componentA.vue";
+import componentB from "./components/componentB.vue";
+export default {
+  name: "App",
+  components: {
+    componentA,
+    componentB
+  },
+  created() {
+    setTimeout(() => {
+      const path =
+        "/Users/guangfa/Desktop/guangfaMac/guangfa/gfCode/open-vscode-file/examples/vue3/src/components/componentA.vue";
+      fetch(`http://localhost:8080/?file=${path}`);
+    }, 2000);
+  }
+};
+</script>
+
+<style></style>
---
+++ src/webpack.ts
@@ -0,0 +1,12 @@
+import openCodefile from "./index";
+import type { specifiedEditor, srcRoot, onErrorCallback } from "./types";
+
+export function openFileServicePlugin(
+  specifiedEditor: specifiedEditor,
+  srcRoot: srcRoot,
+  onErrorCallback: onErrorCallback
+) {
+    return function ({ app }:any) {
+        app.use('/__open-in-editors', openCodefile(specifiedEditor, srcRoot, onErrorCallback))
+    }
+}
